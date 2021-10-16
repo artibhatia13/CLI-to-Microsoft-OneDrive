40ef1ef228255c5024186c73c1c9ad271a17aec6
Roy2712 <62323627+Roy2712@users.noreply.github.com>
2021-10-17 01:19:12 +0530

    Update ICloudIO.java

--- src/main/java/com/ziroh/onedriveproject/ICloudIO.java
+++ src/main/java/com/ziroh/onedriveproject/ICloudIO.java
@@ -7,4 +7,5 @@ public interface ICloudIO {
 
 	FutureTask<Result> renameFile(GraphServiceClient api, String fileId, String newName);
 	FutureTask<FileUploadResult> uploadFile(GraphServiceClient graphClient, String filePath, String onedrivepath);
+	FutureTask<Result> downloadFile(GraphServiceClient graphClient, String fileId);
 }
